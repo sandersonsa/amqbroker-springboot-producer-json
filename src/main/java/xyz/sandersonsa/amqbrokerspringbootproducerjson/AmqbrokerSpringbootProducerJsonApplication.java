35bf664f6071708e02677bb0d162c5173573b5f0
Sanderson Sa <costadesa@gmail.com>
2022-05-25 14:12:14 -0300

    ajustar parse

--- src/main/java/xyz/sandersonsa/amqbrokerspringbootproducerjson/AmqbrokerSpringbootProducerJsonApplication.java
+++ src/main/java/xyz/sandersonsa/amqbrokerspringbootproducerjson/AmqbrokerSpringbootProducerJsonApplication.java
@@ -1,13 +1,28 @@
 package xyz.sandersonsa.amqbrokerspringbootproducerjson;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @SpringBootApplication
 public class AmqbrokerSpringbootProducerJsonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmqbrokerSpringbootProducerJsonApplication.class, args);
+	}
+
+	@Bean
+	@Primary
+	public ObjectMapper objectMapper() {
+		ObjectMapper mapper = JsonMapper.builder()
+        .findAndAddModules()
+        .build();
+		return mapper;
 	}
 
 }
